34381b25f108b27147a894d52ddcd0fcf0a4146a
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2013-09-07 13:42:32 +0900

    Add spec for recurrence check.

--- spec/cal_spec.rb
+++ spec/cal_spec.rb
@@ -69,4 +69,76 @@ describe Event do
       expect(subject.end_date).to eq(end_date)
     end
   end
+
+  describe '#check_recurrent' do
+    let(:date) { Date.new(2013, 9, 4) } # Wed
+    let(:start_date) { Time.new(2013, 9, 4, 9) }
+    let(:end_date) { Time.new(2013, 9, 4, 12) }
+
+    context 'with weekly event' do
+      let(:reccurence_str) { 'FREQ=WEEKLY;INTERVAL=1' }
+
+      it 'returns true for the starting date' do
+        expect(subject.check_recurrent(date)).to be_true
+      end
+
+      it 'returns true for 1 week later' do
+        expect(subject.check_recurrent(date + 1.week)).to be_true
+      end
+
+      it 'returns false for 1 week ago' do
+        expect(subject.check_recurrent(date - 1.week)).to be_false
+      end
+    end
+
+    context 'with weekly event with byday' do
+      let(:reccurence_str) { 'FREQ=WEEKLY;INTERVAL=1;BYDAY=MO,WE,SA' }
+
+      it 'returns true for the starting date' do
+        expect(subject.check_recurrent(date)).to be_true
+      end
+
+      it 'returns false for day in the week and before the starting date' do
+        expect(subject.check_recurrent(date - 2.days)).to be_false
+      end
+
+      it 'returns true for day in the week and after the starting date' do
+        expect(subject.check_recurrent(date + 3.days)).to be_true
+      end
+
+      it 'returns false for another day in the week' do
+        expect(subject.check_recurrent(date + 1.day)).to be_false
+      end
+
+      it 'returns true for days in the next week' do
+        expect(subject.check_recurrent(date + 1.week - 2.days)).to be_true
+        expect(subject.check_recurrent(date + 1.week)).to be_true
+        expect(subject.check_recurrent(date + 1.week + 3.days)).to be_true
+      end
+
+      it 'returns false for 1 week ago' do
+        expect(subject.check_recurrent(date - 1.week)).to be_false
+      end
+    end
+
+    context 'with biweekly event' do
+      let(:reccurence_str) { 'FREQ=WEEKLY;INTERVAL=2' }
+
+      it 'returns true for the starting date' do
+        expect(subject.check_recurrent(date)).to be_true
+      end
+
+      it 'returns false for 1 week later' do
+        expect(subject.check_recurrent(date + 1.week)).to be_false
+      end
+
+      it 'returns true for 2 weeks later' do
+        expect(subject.check_recurrent(date + 2.weeks)).to be_true
+      end
+
+      it 'returns false for 2 weeks ago' do
+        expect(subject.check_recurrent(date - 2.weeks)).to be_false
+      end
+    end
+  end
 end
